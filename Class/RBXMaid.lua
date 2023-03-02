25646dda6d67c63cc5d699e655e02b7a0a5b0904
Curio <116847106+CuriousCurio@users.noreply.github.com>
2023-03-01 20:01:28 -0700

    Update RBXMaid.lua

--- Class/RBXMaid.lua
+++ Class/RBXMaid.lua
@@ -1,10 +1,14 @@
-local typeof = function(a)
-	local meta = getmetatable(a);
-	if type(meta) ~= "table" or meta.__type == nil then return typeof(a); end
-	if type(meta.__type) == "function" then return meta.__type(a); end
-	return meta.__type;
-end
+local function Destroy(a) a:Destroy(); end
+local function Disconnect(a) a:Disconnect(); end
 
+local function DestroyNext(a)
+	if (type(a) == "table" or typeof(a) == "Instance") then return pcall(Destroy, a), nil; end
+	return false;
+end
+local function DisconnectNext(a)
+	if (type(a) == "table" or typeof(a) == "RBXScriptConnection") then return pcall(Disconnect, a), nil; end
+	return false;
+end
 local function RecurseDescendants(table:{}, Fx:(any))
 	if (type(table) ~= "table") then return; end
 	for i,v in pairs(table) do
@@ -13,32 +17,26 @@ local function RecurseDescendants(table:{}, Fx:(any))
 		end
 	end
 end
-local function DestroyNext(a)
-	local type_a = typeof(a);
-	if (type_a == 'RBXMaid' or type_a == "ClassInstance" or type_a == "Instance") then a:Destroy(); return false; end
-	return true;
+
+local function DestroyAll(table)
+	RecurseDescendants(table, DestroyNext);
 end
-local function DisconnectNext(a)
-	local type_a = typeof(a);
-	if (type_a == 'RBXMaid' or (type_a == "RBXScriptSignal" and type(a) == "table") or type_a == "RBXScriptConnection") then a:Disconnect(); return false; end
-	return true;
+local function DisconnectAll(table)
+	RecurseDescendants(table, DisconnectNext);
 end
 
 
 
 local Maid = {};
-Maid.__index = Maid;
+Maid.__index = {
+	Destroy = DestroyAll,
+	Disconnect = DisconnectAll
+}
 
-Maid.Destroy = function(self)
-	RecurseDescendants(self, function(a:any)return DestroyNext(a); end);
-end
-Maid.Disconnect = function(self)
-	RecurseDescendants(self, function(a:any)return DisconnectNext(a); end);
-end
-
-function Maid.new(table:{})
-	local self = setmetatable(table or {}, Maid);
+function Maid.new()
+	local self = setmetatable({}, Maid);
 	return self;
 end
+Maid.__index.new = Maid.new;
 
 return Maid;
