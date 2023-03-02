6495b6e6721e6bff2a200f065350d6f49a94bd11
Curio <116847106+CuriousCurio@users.noreply.github.com>
2023-03-01 20:02:41 -0700

    Update TypeMix.lua

--- Class/TypeMix.lua
+++ Class/TypeMix.lua
@@ -43,66 +43,64 @@ local Enum__eq = function(a, b) -- Checks two values if enum are equal
 	end
 end
 
-local PUBLIC = {
-	Include = function(self, v)
-		rawset(self, EncodeType(v), true);
-	end,
-	IncludeTuple = function(self, table:{})
-		local ni = 1;
-		while (true) do
-			local v = table[ni];
-			if (v == nil and ni > 1 and table[ni-1] == nil) then break; end -- If nil for 2 indicies, then break the loop
-			self:Include(v);
-			ni += 1;
-		end
-	end,
-	Exclude = function(self, __type)
-		rawset(self, __type, nil);
-	end,
-	Iterate = function(self)
-		return pairs(self);
-	end,
-	Clear = function(self)
-		table.clear(self);
-	end,
-	VerifyType = function(self, v)
-		if (rawget(self, typeof(v))) then return true; end
-		for __type in pairs(self) do
-			if (v == __type) then return true; end			 -- If object is equal
-			if (__type == v) then return true; end			 -- if object is equal    (Using both for __eq(a,b) metamathod)
-			if (Enum__eq(__type, v)) then return true; end
-		end
-		return false;
-	end,
-	Verify = function(self, v:any) -- Verifies the inserted value         
-		if (not self:VerifyType(v)) then return false; end
-		local has_func = false;
-		for __type in pairs(self) do
-			if (type(__type) == "function") then
-				has_func = true;
-				if (__type(v)) then return true; end
-			end
-		end
-		if (not has_func) then return true; end
-		return false;
-	end,
-	Copy = function(self) -- Formats the property type, with each property/class seperated by a "/"
-		local table:{}, ni = {}, 1;
-		for v in pairs(self) do
-			table[ni],ni = tostring(v), ni+1;
-		end
-		return table;
-	end,
-	Concat = function(self, sep:'', none:''|nil) -- Converts all contents into a readable string, each type is sepereated by 'sep'
-		local tb = self:Copy();
-		if (#tb == 0) then	return none or sep; end
-		return table.concat(tb, sep);
-	end
-};
-
 
 local TypeMix = {
-	__index = PUBLIC,
+	__index = {
+		Include = function(self, v)
+			rawset(self, EncodeType(v), true);
+		end,
+		IncludeTuple = function(self, table:{})
+			local ni = 1;
+			while (true) do
+				local v = table[ni];
+				if (v == nil and ni > 1 and table[ni-1] == nil) then break; end -- If nil for 2 indicies, then break the loop
+				self:Include(v);
+				ni += 1;
+			end
+		end,
+		Exclude = function(self, __type)
+			rawset(self, __type, nil);
+		end,
+		Iterate = function(self)
+			return pairs(self);
+		end,
+		Clear = function(self)
+			table.clear(self);
+		end,
+		VerifyType = function(self, v)
+			if (rawget(self, typeof(v))) then return true; end
+			for __type in pairs(self) do
+				if (v == __type) then return true; end			 -- If object is equal
+				if (__type == v) then return true; end			 -- if object is equal    (Using both for __eq(a,b) metamathod)
+				if (Enum__eq(__type, v)) then return true; end
+			end
+			return false;
+		end,
+		Verify = function(self, v:any) -- Verifies the inserted value         
+			if (not self:VerifyType(v)) then return false; end
+			local has_func = false;
+			for __type in pairs(self) do
+				if (type(__type) == "function") then
+					has_func = true;
+					if (__type(v)) then return true; end
+				end
+			end
+			if (not has_func) then return true; end
+			return false;
+		end,
+		Copy = function(self) -- Formats the property type, with each property/class seperated by a "/"
+			local table:{}, ni = {}, 1;
+			for v in pairs(self) do
+				table[ni],ni = tostring(v), ni+1;
+			end
+			return table;
+		end,
+		Concat = function(self, sep:'', none:''|nil) -- Converts all contents into a readable string, each type is sepereated by 'sep'
+			local tb = self:Copy();
+			if (#tb == 0) then	return none or sep; end
+			return table.concat(tb, sep);
+		end
+	},
 	__newindex = function()end,
 	__eq = function(a, b)
 		if (rawequal(a, b)) then return true; end
